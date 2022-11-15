723ae135ff75982c35cf7cf0e10162e66ddd2e06
Carter Dodd <cdodd@nanohmics.com>
2022-11-15 15:37:20 -0600

    Fix sc subproject, update pyproj api

--- pkgaux/__init__.py
+++ pkgaux/__init__.py
@@ -9,18 +9,6 @@ def prep( self, logger ):
   with open('VERSION', 'r') as fp:
     self.project.version = fp.read().strip()
 
-  for k in ['openmp']:
-    self.meson.options[k] = self.config[k]
-
-  if self.config.debug:
-
-    self.meson.setup_args += [
-      '--buildtype', 'debug']
-
-  else:
-    self.meson.setup_args += [
-      '--buildtype', 'release',
-      '--optimization', '3']
 
   cc = os.environ.get('CC', '')
   cxx = os.environ.get('CXX', '')
@@ -39,16 +27,12 @@ def prep( self, logger ):
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def dist_binary_prep( self, logger ):
+  build_dir = self.targets[0].build_dir 
 
   # update build number and build (feature) tags
   # NOTE: BUILDNUMBER file doesn't exist until after meson.build is run
-  with open(osp.join(self.meson.build_dir, 'BUILDNUMBER'), 'r') as fp:
+  with open( build_dir / 'BUILDNUMBER', 'r') as fp:
     build_number = int(fp.read().strip())
 
-  with open(osp.join(self.meson.build_dir, 'BUILDTAGS'), 'r') as fp:
-    build_tags = fp.read().strip().split(' ')
-
   self.binary.build_number = build_number
-
-  self.binary.build_suffix = '_'.join([ tag for tag in build_tags if tag])
 
