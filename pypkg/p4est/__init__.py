60a0cec92cf949a7e48337303a8a44fe302224ca
Carter Dodd <cdodd@nanohmics.com>
2023-01-01 23:28:55 -0600

    Optimize _update_iter

--- pypkg/p4est/__init__.py
+++ pypkg/p4est/__init__.py
@@ -1,10 +1,21 @@
+from pathlib import Path
+import ctypes
+
 from .core import P4est
 from .mesh import QuadMesh
 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def get_include():
-  import os.path as osp
-  return osp.join(osp.dirname(__file__), 'core', 'include')
+  return Path(__file__).parent / 'core' / 'include'
 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def get_library():
-  import os.path as osp
-  return osp.join(osp.dirname(__file__), 'core', 'lib')
+  return Path(__file__).parent / 'core' / 'lib'
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def get_libsc():
+  return ctypes.util.find_library(get_library() / 'libsc')
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def get_libp4est():
+  return ctypes.util.find_library(get_library() / 'libp4est')
--- pypkg/p4est/core/_p4est.pyx
+++ pypkg/p4est/core/_p4est.pyx
@@ -7,7 +7,7 @@ from collections.abc import (
   Sequence,
   Mapping )
 
-cimport numpy as cnp
+cimport numpy as npy
 import numpy as np
 
 from mpi4py import MPI
@@ -139,153 +139,63 @@ cdef class P4est:
     self._update_iter()
 
   #-----------------------------------------------------------------------------
-  # @cython.boundscheck(False)
-  # @cython.wraparound(False)
   cdef _update_iter(P4est self):
-    cdef p4est_ghost_t* ghost = NULL
-    cdef p4est_mesh_t* mesh = NULL
+    cdef:
+      p4est_ghost_t* ghost = p4est_ghost_new(
+        self._p4est,
+        P4EST_CONNECT_FULL)
 
-
-    cdef p4est_tree_t* trees = <p4est_tree_t*>self._p4est.trees.array
-    cdef p4est_tree_t* tree = NULL
-
-    cdef p4est_quadrant_t* quads = NULL
-    cdef p4est_quadrant_t* cell = NULL
-    cdef aux_quadrant_data_t* cell_aux
-
-    cdef cnp.npy_intp cell_idx = 0
-    cdef cnp.npy_intp face_idx = 0
-    cdef cnp.npy_intp cell_adj_idx = 0
-    cdef cnp.npy_intp cell_adj_face_idx = 0
-
-    ghost = p4est_ghost_new(
-      self._p4est,
-      P4EST_CONNECT_FULL)
-
-    mesh = p4est_mesh_new_ext(
-      self._p4est,
-      ghost,
-      # compute_tree_index
-      0,
-      # compute_level_lists
-      0,
-      P4EST_CONNECT_FULL)
-
-    cdef np.ndarray[cnp.npy_int32, ndim = 3] quad_to_quad = ndarray_from_ptr(
-      write = False,
-      dtype = np.int32,
-      count = 4*mesh.local_num_quadrants,
-      arr = <char*>mesh.quad_to_quad).reshape(-1,2,2)
-
-    cdef np.ndarray[cnp.npy_int8, ndim = 3] quad_to_face = ndarray_from_ptr(
-      write = False,
-      dtype = np.int8,
-      count = 4*mesh.local_num_quadrants,
-      arr = <char*>mesh.quad_to_face).reshape(-1,2,2)
-
-    cdef np.ndarray[cnp.npy_int32, ndim = 2] quad_to_half = ndarray_from_sc_array(
-      write = False,
-      dtype = np.int32,
-      subitems = 2,
-      arr = mesh.quad_to_half).reshape(-1,2)
+      p4est_mesh_t* mesh = p4est_mesh_new_ext(
+        self._p4est,
+        ghost,
+        # compute_tree_index
+        0,
+        # compute_level_lists
+        0,
+        P4EST_CONNECT_FULL)
 
     prev_leaf_info = self._leaf_info
-    cdef np.ndarray[cnp.npy_int8, ndim = 1] prev_adapt = prev_leaf_info.adapt
-
     self._leaf_info = QuadInfo(mesh.local_num_quadrants)
-    cdef np.ndarray[cnp.npy_int32, ndim = 1] root = self._leaf_info.root
-    cdef np.ndarray[cnp.npy_int8, ndim = 1] level = self._leaf_info.level
-    cdef np.ndarray[cnp.npy_int32, ndim = 2] origin = self._leaf_info.origin
-    cdef np.ndarray[cnp.npy_int32, ndim = 1] weight = self._leaf_info.weight
-    cdef np.ndarray[cnp.npy_int8, ndim = 1] adapt = self._leaf_info.adapt
-    cdef np.ndarray[cnp.npy_int32, ndim = 4] cell_adj = self._leaf_info.cell_adj
-    cdef np.ndarray[cnp.npy_int8, ndim = 3] cell_adj_face = self._leaf_info.cell_adj_face
 
     from_mask = np.ones(len(prev_leaf_info), dtype = bool)
     to_mask = np.ones(len(self._leaf_info), dtype = bool)
 
-    for root_idx in range(self._p4est.first_local_tree, self._p4est.last_local_tree+1):
-      tree = &trees[root_idx]
-      quads = <p4est_quadrant_t*>tree.quadrants.array
-
-      for q in range(tree.quadrants.elem_count):
-        cell = &quads[q]
-        cell_aux = <aux_quadrant_data_t*>cell.p.user_data
-
-        cell_idx = tree.quadrants_offset + q
-        # print(f"idx= {cell_aux.idx}, adapted= {cell_aux.adapted}, adapt_idx= {cell_aux.adapt_idx}")
-
-        # finish tracking of changes due to refine/coarsen operation
-        # now that the indices are known, and thus able to map previous quadrants
-        if cell_aux.adapted == 1:
-          adapt_idx, k = divmod(cell_aux.adapt_idx, 4)
-          i, j = divmod(k, 2)
-          self._leaf_adapt_fine[adapt_idx, i, j] = cell_idx
-
-          prev_cell_idx = self._leaf_adapt_coarse[adapt_idx]
-          adapt[cell_idx] = prev_adapt[prev_cell_idx] - 1
-
-          from_mask[prev_cell_idx] = False
-          to_mask[cell_idx] = False
-
-        elif cell_aux.adapted == -1:
-          self._leaf_adapt_coarse[cell_aux.adapt_idx] = cell_idx
-
-          prev_cell_idx = self._leaf_adapt_fine[cell_aux.adapt_idx]
-          adapt[cell_idx] = 1 + np.amax(prev_adapt[prev_cell_idx])
-
-          from_mask[prev_cell_idx] = False
-          to_mask[cell_idx] = False
-
-        else:
-          #??
-          pass
-
-        # reset auxiliary information
-        cell_aux.idx = cell_idx
-        cell_aux.adapt_idx = -1
-        cell_aux.adapted = 0
-
-        root[cell_idx] = root_idx
-        level[cell_idx] = cell.level
-        origin[cell_idx] = (cell.x, cell.y)
-
-        # get adjacency information from the mesh
-        for i in range(2):
-          for j in  range(2):
-            cell_adj_idx = quad_to_quad[cell_idx,i,j]
-            cell_adj_face_idx = quad_to_face[cell_idx,i,j]
-
-            # A value of v = 0..7 indicates one same-size neighbor.
-            # A value of v = 8..23 indicates a double-size neighbor.
-            #   8 % 8 -> face 0
-            #   8 // 16 -> subface 0
-            #   15 % 8 -> face 7
-            #   15 // 16 -> subface 0
-            # designates the subface of the large neighbor that the quadrant
-            # touches (this is the same as the large neighbor's face corner).
-            #   16 % 8 -> face 0
-            #   16 // 16 -> subface 1
-            #   23 % 8 -> face 7
-            #   23 // 16 -> subface 1
-            # A value of v = -8..-1 indicates two half-size neighbors.
-            #   -8 % 8 -> 0
-            #   -1 % 8 -> 7
-            # TODO: store face orientation separatly (r * 4 + nf)
-            # TODO: store double-size neighbor subface somewhere
-            cell_adj_face[cell_idx,i,j] = cell_adj_face_idx % 8
-
-            if cell_adj_face_idx >= 0:
-              cell_adj[cell_idx,i,j] = cell_adj_idx
-            else:
-              # In this case the corresponding quad_to_quad index points into the
-              # quad_to_half array that stores two quadrant numbers per index,
-              cell_adj[cell_idx,i,j] = quad_to_half[cell_adj_idx]
-
+    _extract_leaf_info(
+      trees = <p4est_tree_t*>self._p4est.trees.array,
+      first_local_tree = self._p4est.first_local_tree,
+      last_local_tree = self._p4est.last_local_tree,
+      quad_to_quad = ndarray_from_ptr(
+        write = False,
+        dtype = np.int32,
+        count = 4*mesh.local_num_quadrants,
+        arr = <char*>mesh.quad_to_quad).reshape(-1,2,2),
+      quad_to_face = ndarray_from_ptr(
+        write = False,
+        dtype = np.int8,
+        count = 4*mesh.local_num_quadrants,
+        arr = <char*>mesh.quad_to_face).reshape(-1,2,2),
+      quad_to_half = ndarray_from_sc_array(
+        write = False,
+        dtype = np.int32,
+        subitems = 2,
+        arr = mesh.quad_to_half).reshape(-1,2),
+      # out
+      root = self._leaf_info.root,
+      level = self._leaf_info.level,
+      origin = self._leaf_info.origin,
+      weight = self._leaf_info.weight,
+      adapt = self._leaf_info.adapt,
+      cell_adj = self._leaf_info.cell_adj,
+      cell_adj_face = self._leaf_info.cell_adj_face,
+      from_mask = from_mask,
+      to_mask = to_mask,
+      prev_adapt = prev_leaf_info.adapt,
+      leaf_adapt_fine = self._leaf_adapt_fine,
+      leaf_adapt_coarse = self._leaf_adapt_coarse )
 
     if len(prev_leaf_info) > 0:
       # copy data that should not change
-      adapt[to_mask] = prev_adapt[from_mask]
+      self._leaf_info.adapt[to_mask] = prev_leaf_info.adapt[from_mask]
 
     p4est_mesh_destroy(mesh)
 
@@ -423,11 +333,11 @@ cdef class P4est:
 
     leaf_adapt_coarse = -np.ones(
       (num_refine,),
-      dtype = np.intp)
+      dtype = np.int32)
 
     leaf_adapt_fine = -np.ones(
       (num_refine, 2, 2),
-      dtype = np.intp )
+      dtype = np.int32 )
 
     self._leaf_adapt_idx = 0
     self._leaf_adapt_coarse = leaf_adapt_coarse
@@ -473,11 +383,11 @@ cdef class P4est:
 
     leaf_adapt_coarse = -np.ones(
       (num_coarse,),
-      dtype = np.intp)
+      dtype = np.int32)
 
     leaf_adapt_fine = -np.ones(
       (num_coarse, 2, 2),
-      dtype = np.intp )
+      dtype = np.int32 )
 
     self._leaf_adapt_idx = 0
     self._leaf_adapt_coarse = leaf_adapt_coarse
@@ -559,6 +469,145 @@ cdef class P4est:
         allow_coarsening,
         <p4est_weight_t>_weight_quadrant )
 
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+cdef void _extract_leaf_info(
+  const p4est_tree_t* trees,
+  npy.npy_int32 first_local_tree,
+  npy.npy_int32 last_local_tree,
+  const npy.npy_int32[:,:,:] quad_to_quad,
+  const npy.npy_int8[:,:,:] quad_to_face,
+  const npy.npy_int32[:,:] quad_to_half,
+  # out
+  npy.npy_int32[:] root,
+  npy.npy_int8[:] level,
+  npy.npy_int32[:,:] origin,
+  npy.npy_int32[:] weight,
+  npy.npy_int8[:] adapt,
+  npy.npy_int32[:,:,:,:] cell_adj,
+  npy.npy_int8[:,:,:] cell_adj_face,
+  npy.npy_bool[:] from_mask,
+  npy.npy_bool[:] to_mask,
+  npy.npy_int8[:] prev_adapt,
+  npy.npy_int32[:,:,:] leaf_adapt_fine,
+  npy.npy_int32[:] leaf_adapt_coarse ) nogil:
+  """Low-level method to copy quadrant state to contiguous arrays.
+  """
+
+  cdef:
+    p4est_tree_t* tree = NULL
+    p4est_quadrant_t* quads = NULL
+    p4est_quadrant_t* cell = NULL
+    aux_quadrant_data_t* cell_aux
+
+    npy.npy_int32 i = 0
+    npy.npy_int32 j = 0
+    npy.npy_int32 k = 0
+    npy.npy_int32 q = 0
+
+    npy.npy_int32 root_idx = 0
+    npy.npy_int32 adapt_idx = 0
+    npy.npy_int8 max_prev_adapt = 0
+
+    npy.npy_int32 cell_idx = 0
+
+    npy.npy_int32 prev_cell_idx = 0
+    npy.npy_int32[:,:] prev_fine_idx
+
+    npy.npy_int32 cell_adj_idx = 0
+    npy.npy_int8 cell_adj_face_idx = 0
+
+  for root_idx in range(first_local_tree, last_local_tree+1):
+    tree = &trees[root_idx]
+    quads = <p4est_quadrant_t*>tree.quadrants.array
+
+    for q in range(tree.quadrants.elem_count):
+      cell = &quads[q]
+      cell_aux = <aux_quadrant_data_t*>cell.p.user_data
+
+      cell_idx = tree.quadrants_offset + q
+      # print(f"idx= {cell_aux.idx}, adapted= {cell_aux.adapted}, adapt_idx= {cell_aux.adapt_idx}")
+
+      # finish tracking of changes due to refine/coarsen operation
+      # now that the indices are known, and thus able to map previous quadrants
+      if cell_aux.adapted == 1:
+        adapt_idx = cell_aux.adapt_idx // 4
+        k = cell_aux.adapt_idx % 4
+
+        i = k // 2
+        j = k % 2
+
+        leaf_adapt_fine[adapt_idx, i, j] = cell_idx
+
+        prev_cell_idx = leaf_adapt_coarse[adapt_idx]
+        adapt[cell_idx] = prev_adapt[prev_cell_idx] - 1
+
+        from_mask[prev_cell_idx] = False
+        to_mask[cell_idx] = False
+
+      elif cell_aux.adapted == -1:
+        leaf_adapt_coarse[cell_aux.adapt_idx] = cell_idx
+
+        prev_fine_idx[:,:] = leaf_adapt_fine[cell_aux.adapt_idx]
+
+        max_prev_adapt = -128
+
+        for i in range(2):
+          for j in  range(2):
+            prev_cell_idx = prev_fine_idx[i,j]
+            max_prev_adapt = max(max_prev_adapt, prev_adapt[prev_cell_idx])
+            from_mask[prev_cell_idx] = False
+
+        adapt[cell_idx] = 1 + max_prev_adapt
+        to_mask[cell_idx] = False
+
+      else:
+        #??
+        pass
+
+      # reset auxiliary information
+      cell_aux.idx = cell_idx
+      cell_aux.adapt_idx = -1
+      cell_aux.adapted = 0
+
+      root[cell_idx] = root_idx
+      level[cell_idx] = cell.level
+      origin[cell_idx,0] = cell.x
+      origin[cell_idx,1] = cell.y
+
+      # get adjacency information from the mesh
+      for i in range(2):
+        for j in  range(2):
+          cell_adj_idx = quad_to_quad[cell_idx,i,j]
+          cell_adj_face_idx = quad_to_face[cell_idx,i,j]
+
+          # A value of v = 0..7 indicates one same-size neighbor.
+          # A value of v = 8..23 indicates a double-size neighbor.
+          #   8 % 8 -> face 0
+          #   8 // 16 -> subface 0
+          #   15 % 8 -> face 7
+          #   15 // 16 -> subface 0
+          # designates the subface of the large neighbor that the quadrant
+          # touches (this is the same as the large neighbor's face corner).
+          #   16 % 8 -> face 0
+          #   16 // 16 -> subface 1
+          #   23 % 8 -> face 7
+          #   23 // 16 -> subface 1
+          # A value of v = -8..-1 indicates two half-size neighbors.
+          #   -8 % 8 -> 0
+          #   -1 % 8 -> 7
+          # TODO: store face orientation separatly (r * 4 + nf)
+          # TODO: store double-size neighbor subface somewhere
+          cell_adj_face[cell_idx,i,j] = cell_adj_face_idx % 8
+
+          if cell_adj_face_idx >= 0:
+            cell_adj[cell_idx,i,j] = cell_adj_idx
+          else:
+            # In this case the corresponding quad_to_quad index points into the
+            # quad_to_half array that stores two quadrant numbers per index,
+            cell_adj[cell_idx,i,j] = quad_to_half[cell_adj_idx]
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # NOTE: these are not class members because they need to match the callback sig.
